1dfb062458ace476086bed4e1dbd15b541e28846
Lily Lyth <78177091+Lyth2@users.noreply.github.com>
2021-08-12 21:58:18 +0000

    fixed problem on the if statment, added points so if a user get it right they get a point

--- index.js
+++ index.js
@@ -4,9 +4,12 @@
 //Due - Monday 30 August
 
 //Explanation of how the game works
-console.log('Welcoe to the card game! A card will be delt and you will have to guess if the next card that is delt will be higher or lower. If what you put is true you will get a point. First to 3 points wins!')
+console.log('Welcome to the card game! A card will be delt and you will have to guess if the next card that is delt will be higher or lower. If what you put is true you will get a point. First to 3 points wins!')
 
-let number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+let point1 = 0;
+let point2 = 0;
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 let length = number.length
 let tableNumber1 = Math.floor(Math.random() * length + 1);
@@ -14,17 +17,23 @@ let tableNumber2 = Math.floor(Math.random() * length + 1);
 
 console.log('The card that has been delt is ' + tableNumber1)
 
-let higher = ' '
-let lower = ' '
+
 
 let player1 = prompt('Player 1 do you think the next card will be higher or lower?')
 
 console.log('the next card is ' + tableNumber2)
 
-if (tableNumber1 > tableNumber2 || player1 == lower){
-  console.log('u didnt fail good job')
-} else if (tableNumber1 < tableNumber2 || player1 == higher){
+if (tableNumber1 > tableNumber2 && player1 == 'higher'){
+  console.log('u fail good job')
+} else if (tableNumber1 < tableNumber2 && player1 == 'higher'){
   console.log('u got it right')
-}else {
+  point1++
+  console.log('Player 1 you now have ' + point1 + ' points' )
+} else if (tableNumber1 > tableNumber2 && player1 == 'lower'){
+  console.log('u got it right')
+  point1++
+  console.log('player 1 you now have ' + point1 + ' points')
+} else if (tableNumber1 < tableNumber2 && player1 == 'lower'){
   console.log('lol u got it wrong')
 }
+
