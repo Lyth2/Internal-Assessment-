5c034793b84144620400a427a1c003d25410780c
Lily Lyth <78177091+Lyth2@users.noreply.github.com>
2021-08-28 08:18:55 +0000

    fixed some spelling and added so that the players put in a name they want to be called trough out the game

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 let length = number.length
 
-
+//loop, so it keeps going untill one of the players gets 3 points
 do{
 
 play();
 function play(){
+
+//asks for the players names, so now insted of calling them player 1 or player 2 it will call them the names they input.
+p1N = prompt('player 1 what is your name?')
+p2N = prompt('player 2 what is your name?')
 
 //picks a random number 
 let tableNumber1 = Math.floor(Math.random() * length + 1);
@@ -28,25 +32,25 @@ let tableNumber2 = Math.floor(Math.random() * length + 1);
 console.log('The card that has been dealt is ' + tableNumber1)
 
 //player 1 guesses if the next card will be higher or lower
-let player1 = prompt('Player 1 do you think the next card will be higher or lower?')
+let player1 = prompt(p1N + ' do you think the next card will be higher or lower?')
 
 console.log('the next card is ' + tableNumber2)
 
 //determains what happens if they input the right or wrong thing
 if (tableNumber1 > tableNumber2 && player1 == 'higher'){
-  console.log('u fail good job, you get 0 points')
-  console.log('player 1 you now have ' + point1 + ' points')
+  console.log(p1N + ' you failed good job, you now have ' + point1 + ' points')
+  
 } else if (tableNumber1 < tableNumber2 && player1 == 'higher'){
-  console.log('u got it right')
   point1++
-  console.log('Player 1 you now have ' + point1 + ' points' )
+  console.log(p1N + ' you got it right, you now have ' + point1 + ' points')
+
 } else if (tableNumber1 > tableNumber2 && player1 == 'lower'){
-  console.log('u got it right')
   point1++
-  console.log('player 1 you now have ' + point1 + ' points')
+  console.log(p1N + ' u got it right, you now have ' + point1 + ' points')
+  
 } else if (tableNumber1 < tableNumber2 && player1 == 'lower'){
-  console.log('lol u got it wrong, you get 0 points')
-  console.log('player 1 you now have ' + point1 + ' points')
+  console.log(p1N + ' you got it wrong, you now have ' + point1 + ' points')
+  
 }
 
 //reinitialize the random number so it is differnt for player 2
@@ -56,35 +60,36 @@ tableNumber2 = Math.floor(Math.random() * length + 1);
 console.log('The next card that has been dealt is ' + tableNumber1)
 
 //player 2 guesses if the next card will be higher or lower
-let player2 = prompt('Player 2 do you think the next card will be higher or lower?')
+let player2 = prompt(p2N + ' do you think the next card will be higher or lower?')
 
 console.log('the next card is ' + tableNumber2)
 
 //determains what happens with player 2 input
 if (tableNumber1 > tableNumber2 && player2 == 'higher'){
-  console.log('u fail good job, you get 0 points')
-  console.log('player 2 you now have ' + point2 + ' points')
+  console.log(p2N + ' you failed good job, you now have ' + point2 + ' points')
+ 
 } else if (tableNumber1 < tableNumber2 && player2 == 'higher'){
-  console.log('u got it right')
   point2++
-  console.log('Player 2 you now have ' + point2 + ' points' )
+  console.log(p2N + ' you got it right, you now have ' + point2 + ' points')
+
 } else if (tableNumber1 > tableNumber2 && player2 == 'lower'){
-  console.log('u got it right')
   point2++
-  console.log('player 2 you now have ' + point2 + ' points')
+  console.log(p2N + ' you got it right, you now have ' + point2 + ' points')
+  
 } else if (tableNumber1 < tableNumber2 && player2 == 'lower'){
-  console.log('lol u got it wrong, you get 0 points')
-  console.log('player 2 you now have ' + point2 + ' points')
+  console.log(p2N + ' you got it wrong, you now have ' + point2 + ' points')
+  
 }
 
 }
-//the loop stops when ...
+//the loop stops when one of the players gets three points
 }while (point1 < 3 && point2 < 3){
-  console.log('player 2 you win')
+  
+
 } 
 
 //if they want to play again then the loop will play again if not then it will say game over
-let playAgain = prompt('Do you want to play again?')
+let playAgain = prompt(p1N + ' and ' + p2N + ' do you want to play again?')
 
 if(playAgain == 'no'){
   console.log('okay, GAME OVER')
